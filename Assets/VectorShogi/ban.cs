f0cbc4613b4b4f57ee1dea8661905edd8da4e5ae
sumogri <kazusi@hotmail.co.jp>
2018-02-06 00:20:23 +0900

    [add] ざっくりPhoton Unity Networkingで動かす将棋っぽい何か

---
+++ Assets/VectorShogi/ban.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ban : MonoBehaviour,IDropHandler,IEndDragHandler {
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        eventData.pointerDrag.transform.position = transform.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+    }
+}
---
+++ Assets/VectorShogi/koma.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class koma : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    private Vector3 mouseDiff;
+    private CanvasGroup myGroup;
+
+    // Use this for initialization
+    void Start()
+    {
+        myGroup = transform.parent.GetComponent<CanvasGroup>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition + mouseDiff;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        myGroup.blocksRaycasts = true;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        mouseDiff = transform.position - Input.mousePosition;
+        transform.SetAsLastSibling();
+
+        myGroup.blocksRaycasts = false;
+    }
+
+}
