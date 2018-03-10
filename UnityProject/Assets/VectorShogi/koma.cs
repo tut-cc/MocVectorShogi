2399827d4dfd533fbe91eb4b629628e2940c7d77
sumogri <kazusi@hotmail.co.jp>
2018-03-11 03:02:51 +0900

    [add] コマ反転機能と、消去を追加

--- UnityProject/Assets/VectorShogi/koma.cs
+++ UnityProject/Assets/VectorShogi/koma.cs
@@ -9,7 +9,9 @@ public class koma : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHand
     private Vector3 mouseDiff;
     private CanvasGroup myGroup;
     private PhotonView photonView;
+    private bool isDraging = false;
     private static Transform root = null;
+    private static readonly Quaternion Reverce = Quaternion.Euler(0, 0, 180);
     public static Sprite initSprite = null;
 
     // Use this for initialization
@@ -24,6 +26,21 @@ public class koma : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHand
         photonView = transform.GetComponent<PhotonView>();
     }
 
+    private void Update()
+    {
+        
+        if (Input.GetButtonDown("Reverce") && isDraging)
+        {
+            transform.rotation = transform.rotation * koma.Reverce;
+        }
+        if (Input.GetButtonDown("Destroy") && isDraging)
+        {
+            myGroup.blocksRaycasts = true;
+            PhotonNetwork.Destroy(this.photonView);
+        }
+
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         var pos = Input.mousePosition;
@@ -34,6 +51,7 @@ public class koma : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHand
     public void OnEndDrag(PointerEventData eventData)
     {
         myGroup.blocksRaycasts = true;
+        isDraging = false;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -43,5 +61,6 @@ public class koma : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHand
         transform.SetAsLastSibling();
         photonView.RequestOwnership();
         myGroup.blocksRaycasts = false;
+        isDraging = true;
     }    
 }
